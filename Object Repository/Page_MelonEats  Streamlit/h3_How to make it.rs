fff3cd7cce9c97b196e38890151f37492cc7b130
ACER <ACER@LAPTOP-4K8QCVED>
2024-12-24 08:21:19 +0700

    this commit create the branch master for repo learning katalon

---
+++ Object Repository/Page_MelonEats  Streamlit/h3_How to make it.rs
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_How to make it</name>
+   <tag></tag>
+   <elementGuidId>04781919-1dda-4811-8547-2a876cbae714</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h3[@id='how-to-make-it']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#how-to-make-it</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>6c97f375-384c-433e-bc9c-f640f67e707b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>how-to-make-it</value>
+      <webElementGuid>e6d60a90-ab50-4cbe-b5dc-89e3235d9bae</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>How to make it:</value>
+      <webElementGuid>fc5c2149-afb5-4464-b232-b929e4e03fd5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;how-to-make-it&quot;)</value>
+      <webElementGuid>d035ab73-269c-4809-9e81-9762dfbd3e06</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/Page_MelonEats  Streamlit/iframe__iframe_nim44_26</value>
+      <webElementGuid>beabf717-4773-468c-ae12-fa6ef21baf32</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//h3[@id='how-to-make-it']</value>
+      <webElementGuid>a887faf1-208d-494f-8b32-5660fd54ef40</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div/div/div/section/div/div/div/div/div[7]/div/div/div/div[2]/div/div/div/div/div/div/div/h3</value>
+      <webElementGuid>9204a73a-ab98-41dc-9d5f-8c6b4632aafa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ingredients:'])[1]/following::h3[1]</value>
+      <webElementGuid>4bb3b765-60cc-49f3-aaa8-40ec885d26cc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Servings:'])[1]/following::h3[1]</value>
+      <webElementGuid>65b47bb9-9c07-4ed1-91fd-c771224724b1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Steps:'])[1]/preceding::h3[1]</value>
+      <webElementGuid>2ee8cf2e-b987-42fe-add9-50a13a19a22e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='How to make it:']/parent::*</value>
+      <webElementGuid>391b8ee3-bb77-4a13-b204-2cf268122ae1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div/div/div/h3</value>
+      <webElementGuid>f50141bb-1fa0-4f6c-a728-7b4c64a225b0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[@id = 'how-to-make-it' and (text() = 'How to make it:' or . = 'How to make it:')]</value>
+      <webElementGuid>c1cc0e38-def6-4b2b-a98f-519f0b775601</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_MelonEats  Streamlit/p_Fetch Recipes.rs
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_Fetch Recipes</name>
+   <tag></tag>
+   <elementGuidId>c8d47d4c-5ac8-4bf6-bda5-e3682a28ea1e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div/div/div/div/section/div/div/div/div/div[5]/div/button/div/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.st-emotion-cache-1hyd1ho.e121c1cl0 > p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>c87b1c71-2b74-430a-9cf5-22a22c1aaefc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Fetch Recipes</value>
+      <webElementGuid>adde8b75-1c80-4ee9-9189-aab6dd61ba0b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[1]/div[@class=&quot;withScreencast&quot;]/div[@class=&quot;stApp stAppEmbeddingId-soy43kqx38q7 streamlit-wide st-emotion-cache-1r4qj8v e10tffaf0&quot;]/div[@class=&quot;stAppViewContainer appview-container st-emotion-cache-1yiq2ps ekr3hml0&quot;]/section[@class=&quot;stMain st-emotion-cache-bm2z3a ekr3hml1&quot;]/div[@class=&quot;stMainBlockContainer block-container st-emotion-cache-1ibsh2c ekr3hml4&quot;]/div[@class=&quot;st-emotion-cache-0 eiemyj5&quot;]/div[@class=&quot;st-emotion-cache-b95f0i eiemyj4&quot;]/div[@class=&quot;stVerticalBlock st-emotion-cache-jwm1ci eiemyj3&quot;]/div[@class=&quot;stElementContainer element-container st-emotion-cache-7hy365 eiemyj1&quot;]/div[@class=&quot;stButton&quot;]/button[@class=&quot;st-emotion-cache-ocsh0s e1obcldf2&quot;]/div[@class=&quot;st-emotion-cache-1hyd1ho e121c1cl0&quot;]/p[1]</value>
+      <webElementGuid>4528ff8f-7c00-4acc-9248-4e3f9434943f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/Page_MelonEats  Streamlit/iframe__iframe_nim44_26</value>
+      <webElementGuid>6e38e83b-9c31-49a2-b6ac-18d7e9085ec4</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div/div/div/section/div/div/div/div/div[5]/div/button/div/p</value>
+      <webElementGuid>9d5d4292-ecc9-4872-aacc-218ef18588ff</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='MelonEats - Recipe Finder'])[1]/following::p[7]</value>
+      <webElementGuid>0340f66b-2a93-4812-a06c-472e6f1d4101</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Fork'])[1]/following::p[7]</value>
+      <webElementGuid>6b55b8b8-27af-49a8-91e8-5c38f037d0eb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Fetch Recipes']/parent::*</value>
+      <webElementGuid>e1046664-bce1-4001-bf52-916c0d638ecc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button/div/p</value>
+      <webElementGuid>1dbab78a-7240-47cd-baf1-9a922d7fd804</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = 'Fetch Recipes' or . = 'Fetch Recipes')]</value>
+      <webElementGuid>0462fd8f-cc26-4a50-b91d-df2c52b8f10e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
